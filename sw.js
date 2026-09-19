bb09a1787529a53d4aea46bc6d3c609796f8a592
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-09-19 20:27:06 +0200

    Implement service worker for caching files

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
+// GasDrive Click - Service Worker V14+ SBG Master
+const CACHE_NAME = 'gasdrive-click-v14-2026';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './sbg_master.json',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
+self.addEventListener('install', (evt) => {
+  console.log('[SW] Install V14+');
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  console.log('[SW] Activate');
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    })
+  );
+});
